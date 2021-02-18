cf3a80cabcdb89415c36bde983955628cfb3b57b
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-02-18 16:15:44 +0100

    count users updated with migration

--- db/migrate/20210218135141_update_postal_code_registration.rb
+++ db/migrate/20210218135141_update_postal_code_registration.rb
@@ -1,34 +1,35 @@
+# frozen_string_literal: true
+
 class UpdatePostalCodeRegistration < ActiveRecord::Migration[5.2]
   def up
-
     affected_users = users_with_postal_code
-    if affected_users.blank?
-      puts "Update postal code migration : No users affected.
-End of migration"
-      return
-
-    else
-      puts "#{affected_users.count} users will be affected, are you sure ? [y/n]"
-      choice = $stdin.gets.chomp
-
-      if choice.downcase != "y"
-        puts "Migration aborted"
-        return
-      end
-    end
+    updated_users = 0
 
     affected_users&.each do |user|
       next unless user[:address].present? && user[:address]['postal_code'].present?
 
       current_postal_code = user[:address]["postal_code"]
 
-      user[:address]["postal_code"] = reformat_postal_code current_postal_code
-      user.save!
+      if postal_code_need_update? current_postal_code
+        user[:address]["postal_code"] = reformat_postal_code current_postal_code
+        user.save!
+        updated_users += 1
+      end
     end
+
+    puts "#{updated_users} users updated"
   end
+
+  private
 
   def users_with_postal_code
     Decidim::User.where.not(address: [nil, {}])
+  end
+
+  def postal_code_need_update?(postal_code)
+    return false if postal_code.is_a?(String) && postal_code.length != 4
+
+    true
   end
 
   def reformat_postal_code(postal_code)
