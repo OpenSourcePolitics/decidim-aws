695b375bc8a5c01b98a688ac8465e3fcefdf9d65
Armand Fardeau <fardeauarmand@gmail.com>
2021-03-29 16:01:14 +0200

    Set initiative state for deleted author

--- lib/extends/destroy_account_extend.rb
+++ lib/extends/destroy_account_extend.rb
@@ -23,23 +23,8 @@ module DestroyAccountExtend
     end
 
     def manage_user_initiatives
-      Rails.logger.debug "+++++++++++++++++++++++++"
-      Rails.logger.debug "DestroyAccount.manage_user_initiatives"
-      Rails.logger.debug "with @user" if @user
-      Rails.logger.debug @form.to_h
-      Rails.logger.debug "+++++++++++++++++++++++++"
-
       Decidim::Initiative.where(author: @user).each do |initiative|
-        Rails.logger.debug initiative.id
-        Rails.logger.debug initiative.state
-        if %w(created validating accepted).include?(initiative.state)
-          Rails.logger.debug "will be discarded"
-          initiative.update_columns(state: "discarded")
-        else
-          Rails.logger.debug "will be rejected"
-          initiative.update_columns(state: "rejected")
-        end
-        Rails.logger.debug "--"
+        initiative.update_columns(state: "discarded")
       end
 
     end
